0d4a77a88a49018468032f7c4d888906991aa460
Vadim Kosarev <vadim.kosarev@gmail.com>
2024-04-15 20:46:38 +0300

    Initial commit

---
+++ src/test/java/dev/vk/jfc/app/storage/appstorage/AppStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vk.jfc.app.storage.appstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
