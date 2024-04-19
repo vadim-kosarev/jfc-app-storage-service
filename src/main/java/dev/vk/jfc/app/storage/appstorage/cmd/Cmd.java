2cb1b7596776133517c4f375f8161e25b04ecd10
Vadim Kosarev <vadim.kosarev@gmail.com>
2024-04-19 07:16:12 +0300

    Create DB.9

--- src/main/java/dev/vk/jfc/app/storage/appstorage/cmd/Cmd.java
+++ src/main/java/dev/vk/jfc/app/storage/appstorage/cmd/Cmd.java
@@ -1,9 +1,7 @@
 package dev.vk.jfc.app.storage.appstorage.cmd;
 
 import dev.vk.jfc.app.storage.appstorage.dto.Contained;
-import dev.vk.jfc.app.storage.appstorage.dto.Customer;
 import dev.vk.jfc.app.storage.appstorage.repository.ContainedRepository;
-import dev.vk.jfc.app.storage.appstorage.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,21 +16,23 @@ public class Cmd implements CommandLineRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(Cmd.class);
 
-    private final CustomerRepository repository;
     private final ContainedRepository repo2;
 
     @Override
     public void run(String... args) throws Exception {
         Contained cc = Contained.builder()
-                .label("Another record: ... %s".formatted(System.currentTimeMillis()))
+                .label("RECORD LABELED: ... %s".formatted(System.currentTimeMillis()))
                 .build();
         cc = repo2.save(cc);
         logger.info("CC: {}", cc);
 
         Contained c2 = Contained.builder()
                 .label("Another record %s".formatted(new Date()))
+                .parent(cc)
                 .build();
         c2 = repo2.save(c2);
         logger.info("C2: {}", c2);
+
+
     }
 }
--- src/main/java/dev/vk/jfc/app/storage/appstorage/dto/Contained.java
+++ src/main/java/dev/vk/jfc/app/storage/appstorage/dto/Contained.java
@@ -1,26 +1,33 @@
 package dev.vk.jfc.app.storage.appstorage.dto;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
+@Table(name = "_C")
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Contained {
+
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(name = "label")
     private String label;
+
+    @ManyToOne
+    @JoinColumn(name = "parentID", referencedColumnName = "id")
+    private Contained parent;
 
 }
